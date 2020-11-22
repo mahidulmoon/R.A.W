dc9853c640ee28a4b18f73a5c75761f1d2e7c41f
mahidulmoon <mahidulmoon@gmail.com>
2020-11-22 11:56:23 +0600

    empty value processing

--- ML course/data_preprocessing_template.py
+++ ML course/data_preprocessing_template.py
@@ -1,11 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 
 #importing tha dataset
 dataset = pd.read_csv('Data.csv')
 
 #iloc[row,column]
-x = dataset.iloc[:, :].values
+x = dataset.iloc[:, :-1].values #independent values
+y = dataset.iloc[:,-1:].values  #dependent values
+
+
+simp = SimpleImputer(missing_values = 'NaN', strategy = 'mean')
+simp = SimpleImputer().fit(x[:, 1:3]) 
+x[:, 1:3] = simp.transform(x[:, 1:3])
+
+
 print(x)
