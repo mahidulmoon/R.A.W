e327235283cd7a2bc162f9cb917ddb7c5dbeadfc
mahidulmoon <mahidulmoon@gmail.com>
2020-11-22 12:31:25 +0600

    train for test

--- ML course/data_preprocessing_template.py
+++ ML course/data_preprocessing_template.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
 #importing tha dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('G:\python\ML course\Data.csv')
 
 #iloc[row,column]
 x = dataset.iloc[:, :-1].values #independent values
@@ -21,4 +22,8 @@ x[:, 1:3] = simp.transform(x[:, 1:3])
 lablerencoder_x = LabelEncoder()
 x[:,0] = lablerencoder_x.fit_transform(x[:,0])
 
-print(x)
+#print(x)
+
+#spliting dataset into the training
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+print(x_test)
