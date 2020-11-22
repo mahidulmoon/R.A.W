724b3749dfef91159342c490203d8d77dee775f7
mahidulmoon <mahidulmoon@gmail.com>
2020-11-22 11:16:52 +0600

    reading csv and show the values using iloc

--- ML course/data_preprocessing_template.py
+++ ML course/data_preprocessing_template.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+#importing tha dataset
+dataset = pd.read_csv('Data.csv')
+
+#iloc[row,column]
+x = dataset.iloc[:, :].values
+print(x)
---
+++ README.txt
@@ -0,0 +1,3 @@
+## to install library in machine manually run below commad:
+
+pip install numpy matplotlib pandas
